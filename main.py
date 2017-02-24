40dfebc8727ea7ab2c2fefa024c4cd11ecb74d11
pppedd1 <pavan.peddakota@monsanto.com>
2017-02-24 17:52:25 -0600

    build-a-blog

---
+++ main.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+import webapp2
+import jinja2
+
+from google.appengine.ext import db
+
+template_dir=os.path.join((os.path.dirname(__file__)),'templates')
+jinja_env=jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+                             autoescape=True)
+class Handler(webapp2.RequestHandler):
+        def write(self,*a,**kw) :
+            self.response.write(*a,**kw)
+
+        def render_str(self,template,**params):
+            t= jinja_env.get_template(template)
+            return t.render(params)
+
+        def render(self,template,**kw):
+            self.write(self.render_str(template,**kw))
+class Art(db.Model):
+    title=db.StringProperty(required=True)
+    art= db.TextProperty(required=True)
+    created=db.DateTimeProperty(auto_now_add=True)
+
+
+class MainHandler(Handler) :
+      def get(self) :
+          self.render("base.html")
+
+
+
+
+class BlogHandler(Handler):
+
+      def render_blog(self,title="",art="",arts=""):
+           arts=db.GqlQuery("SELECT * FROM Art "
+                 " ORDER BY created DESC LIMIT 5")
+           self.render("main_blog.html",title=title,art=art,arts=arts)
+      def get(self) :
+          self.render_blog("main_blog.html")
+
+class NewPostHandler(Handler):
+
+         def render_newblog(self,title="",art="",error=""):
+              self.render("new_post.html",title=title,art=art,error=error)
+
+         def get(self):
+               self.render_newblog()
+
+
+         def post(self) :
+             title=self.request.get("title")
+             art=self.request.get("art")
+             if title and art :
+                 a=Art(title=title,art=art)
+                 a.put()
+                 art_val=str(a.key().id())
+                 self.redirect("/blog/"+art_val)
+             else :
+                error="we need both a title and some artwork!"
+                self.render_newblog(title,art,error)
+
+class ViewPostHandler(webapp2.RequestHandler) :
+      def get(self,id) :
+
+          artdata=Art.get_by_id(int(id))
+          if artdata :
+             t = jinja_env.get_template("post.html")
+             self.response.out.write(t.render(artdata=artdata))
+          else :
+             self.response.out.write("No such post in the blog!")
+
+
+
+
+app = webapp2.WSGIApplication([
+    ('/',MainHandler),
+    ('/blog',BlogHandler),
+    ('/blog/newpost',NewPostHandler),
+    webapp2.Route('/blog/<id:\d+>', ViewPostHandler)
+], debug=True)
